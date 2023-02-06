9c1428827cdaff6e635bb79fb11b455edcee4b50
Maryor-z <ogundipemayowa90@icloud.com>
2023-02-06 01:43:09 +0100

    JavaFX commit

---
+++ src/main/java/com/example/mycontactsb/ContactController.java
@@ -0,0 +1,44 @@
+package com.example.mycontactsb;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ContactController {
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField phoneNumberField;
+
+    @FXML
+    private TextField notesField;
+
+    public ContactModel getNewContact() {
+        String firstName = firstNameField.getText();
+        String lastName = lastNameField.getText();
+        String phoneNumber = phoneNumberField.getText();
+        String notes = notesField.getText();
+
+        ContactModel newContact = new ContactModel(firstName, lastName, phoneNumber, notes);
+        return newContact;
+    }
+
+    public void editContact(ContactModel contact) {
+        firstNameField.setText(contact.getFirstName());
+        lastNameField.setText(contact.getLastName());
+        phoneNumberField.setText(contact.getPhoneNumber());
+        notesField.setText(contact.getNotes());
+    }
+
+    public void updateContact(ContactModel contact) {
+        contact.setFirstName(firstNameField.getText());
+        contact.setLastName(lastNameField.getText());
+        contact.setPhoneNumber(phoneNumberField.getText());
+        contact.setNotes(notesField.getText());
+    }
+
+}
